954c8e07b23227946b5f616a2abc3943a7b6da91
Keshalem <89782912+Keshalem@users.noreply.github.com>
2021-09-04 12:12:51 +0545

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,11 @@
+print("Hello World")
+a = 4
+b = 6
+c = 7
+d = 5
+
+#Addition
+sum = (a+b)*(c+d)
+sum1 = ((a-c)*b)/d
+print(f"The sum is {sum} and another sum is {sum1}. Bravo!")
+print("The sum is " + str(sum) + " and another sum is " + str(sum1) + ". Great")
